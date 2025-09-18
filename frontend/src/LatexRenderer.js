5b9c048c3ef5d49715aff034ca02ca33c30fe5a0
Patrick Capaldo <patrickxcapaldo@gmail.com>
2025-09-18 22:12:16 +0100

    latex working properly

---
+++ frontend/src/LatexRenderer.js
@@ -0,0 +1,10 @@
+import React, { useRef, useEffect } from 'react';
+import Latex from 'react-latex';
+
+const LatexRenderer = ({ children }) => {
+    // We don't need a ref since `react-latex` handles this, but it's good practice
+    // with other libraries like vanilla KaTeX
+    return <Latex>{children}</Latex>;
+};
+
+export default React.memo(LatexRenderer);
