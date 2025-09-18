54da1267a2163884009753433b80960dacb22311
Patrick Capaldo <patrickxcapaldo@gmail.com>
2025-09-18 22:27:11 +0100

    latex pillar score working

--- frontend/src/LatexRenderer.js
+++ frontend/src/LatexRenderer.js
@@ -1,10 +1,10 @@
 import React, { useRef, useEffect } from 'react';
 import Latex from 'react-latex';
 
-const LatexRenderer = ({ children }) => {
+const LatexRenderer = ({ children, displayMode }) => {
     // We don't need a ref since `react-latex` handles this, but it's good practice
     // with other libraries like vanilla KaTeX
-    return <Latex>{children}</Latex>;
+    return <Latex displayMode={displayMode}>{children}</Latex>;
 };
 
 export default React.memo(LatexRenderer);
